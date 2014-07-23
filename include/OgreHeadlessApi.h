7a2e8ee731e707612983c6f1dd85651077d92052
Jonne Nauha <jonne@adminotech.com>
2014-07-23 10:44:41 +0300

    Fix compilation on non-windows, had not tested this before and was missinga platform include.

--- include/OgreHeadlessApi.h
+++ include/OgreHeadlessApi.h
@@ -16,16 +16,18 @@
 
 #pragma once
 
+#include "OgrePrerequisites.h"
+
 #if (OGRE_PLATFORM == OGRE_PLATFORM_WIN32) && !defined(__MINGW32__) && !defined(OGRE_STATIC_LIB)
-#	ifdef RenderSystem_Headless_EXPORTS
-#		define _OgreHeadlessExport __declspec(dllexport)
-#	else
+#   ifdef RenderSystem_Headless_EXPORTS
+#       define _OgreHeadlessExport __declspec(dllexport)
+#   else
 #       if defined(__MINGW32__)
 #           define _OgreHeadlessExport
 #       else
-#    		define _OgreHeadlessExport __declspec(dllimport)
+#           define _OgreHeadlessExport __declspec(dllimport)
 #       endif
-#	endif
+#   endif
 #elif defined (OGRE_GCC_VISIBILITY)
 #    define _OgreHeadlessExport  __attribute__ ((visibility("default")))
 #else
